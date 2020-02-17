8ae99f701fe8169afc45d83c0afe80308bf0b144
Eddie Skr <eskruolis@gmail.com>
2020-02-17 17:57:47 +0000

    1

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,41 +2,50 @@
 
 namespace App\Controller;
 
-
-use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
-    
     public function show($slug)
     {
-      
         $comments = [
-            'lore ipsunlore ipsun lore ipsun',
-            'lore ipsunlore ipsun lore ipsun',
-            'lore ipsunlore ipsun lore ipsun',
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-  dump($slug);
-
         return $this->render('article/show.html.twig', [
-            'title' => ucwords(str_replace('-', '', $slug)),
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
-       
     }
-    
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
+    }
 }
